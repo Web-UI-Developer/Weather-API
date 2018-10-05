3bf47d4d3a0b3e2b4425f6b7828615753eea8dfa
Web-UI-Developer <sampreet.github@gmail.com>
2018-10-05 01:18:25 -0400

    updated app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const weather = new Weather(weatherLocation.city, weatherLocation.state);
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', getWeather);
+
 document.getElementById('w-change-btn').addEventListener('click', (e) => {
   const city = document.getElementById('city').value;
   const state = document.getElementById('state').value;
